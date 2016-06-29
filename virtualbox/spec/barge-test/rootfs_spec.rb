fd66c357eacb6ddaa1c917c5d931f83114e6f99b
A.I <ailis@paw.zone>
2016-06-29 09:01:00 -0700

    Follow changing the mount point at upstream

--- virtualbox/spec/barge-test/rootfs_spec.rb
+++ virtualbox/spec/barge-test/rootfs_spec.rb
@@ -8,18 +8,22 @@ describe file('/mnt/sda1') do
   it { should be_mounted }
 end
 
+describe file('/mnt/data') do
+  it { should be_mounted }
+end
+
 describe file('/var/lib/docker') do
-  it { should be_linked_to '/mnt/sda1/var/lib/docker' }
+  it { should be_linked_to '/mnt/data/var/lib/docker' }
 end
 
 describe file('/var/log/docker') do
-  it { should be_linked_to '/mnt/sda1/var/log/docker' }
+  it { should be_linked_to '/mnt/data/var/log/docker' }
 end
 
 describe file('/home') do
-  it { should be_linked_to '/mnt/sda1/home' }
+  it { should be_linked_to '/mnt/data/home' }
 end
 
 describe file('/opt') do
-  it { should be_linked_to '/mnt/sda1/opt' }
+  it { should be_linked_to '/mnt/data/opt' }
 end
