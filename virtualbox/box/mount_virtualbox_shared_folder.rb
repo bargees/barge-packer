a354e57eb7ed3c5a3ac8b89268a75bdca04ccb37
A.I <ailis@paw.zone>
2016-09-01 09:56:52 -0700

    Fix a usage of `id -g` and optimize for Barge
    https://github.com/mitchellh/vagrant/pull/7720

--- virtualbox/box/mount_virtualbox_shared_folder.rb
+++ virtualbox/box/mount_virtualbox_shared_folder.rb
@@ -8,49 +8,47 @@ module VagrantPlugins
   module GuestLinux
     module Cap
       class MountVirtualBoxSharedFolder
+        @@logger = Log4r::Logger.new("vagrant::guest::linux::mount_virtualbox_shared_folder")
+
         extend Vagrant::Util::Retryable
 
         def self.mount_virtualbox_shared_folder(machine, name, guestpath, options)
           guest_path = Shellwords.escape(guestpath)
 
-          mount_commands = ["set -e"]
+          @@logger.debug("Mounting #{name} (#{options[:hostpath]} to #{guestpath})")
 
-          if options[:owner].is_a? Integer
+          if options[:owner].to_i.to_s == options[:owner].to_s
             mount_uid = options[:owner]
           else
             mount_uid = "`id -u #{options[:owner]}`"
           end
 
-          if options[:group].is_a? Integer
+          if options[:group].to_i.to_s == options[:group].to_s
             mount_gid = options[:group]
-            mount_gid_old = options[:group]
           else
-            mount_gid = "`getent group #{options[:group]} | cut -d: -f3`"
-            mount_gid_old = "`id -g #{options[:group]}`"
+            if options[:owner] == options[:group]
+              mount_gid = "`id -g #{options[:owner]}`"
+            else
+              mount_gid = "`grep -w ^#{options[:group]} /etc/group | cut -d: -f3`"
+            end
           end
 
-          # First mount command uses getent to get the group
-          mount_options = "-o uid=#{mount_uid},gid=#{mount_gid}"
-          mount_options += ",#{options[:mount_options].join(",")}" if options[:mount_options]
-          mount_commands << "mount -t vboxsf #{mount_options} #{name} #{guest_path} || \\"
-
-          # Second mount command uses the old style `id -g`
-          mount_options = "-o uid=#{mount_uid},gid=#{mount_gid_old}"
-          mount_options += ",#{options[:mount_options].join(",")}" if options[:mount_options]
-          mount_commands << "mount -t vboxsf #{mount_options} #{name} #{guest_path}"
+          mount_options = options.fetch(:mount_options, [])
+          mount_options += ["uid=#{mount_uid}", "gid=#{mount_gid}"]
+          mount_options = mount_options.join(',')
+          mount_command = "mount -t vboxsf -o #{mount_options} #{name} #{guest_path}"
 
           # Create the guest path if it doesn't exist
           machine.communicate.sudo("mkdir -p #{guest_path}")
 
           # Attempt to mount the folder. We retry here a few times because
           # it can fail early on.
-          command = mount_commands.join("\n")
           stderr = ""
           retryable(on: Vagrant::Errors::VirtualBoxMountFailed, tries: 3, sleep: 5) do
-            machine.communicate.sudo(command,
+            machine.communicate.sudo(mount_command,
               error_class: Vagrant::Errors::VirtualBoxMountFailed,
               error_key: :virtualbox_mount_failed,
-              command: command,
+              command: mount_command,
               output: stderr,
             ) { |type, data| stderr = data if type == :stderr }
           end
@@ -58,20 +56,9 @@ module VagrantPlugins
           # Chown the directory to the proper user. We skip this if the
           # mount options contained a readonly flag, because it won't work.
           if !options[:mount_options] || !options[:mount_options].include?("ro")
-            chown_commands = []
-            chown_commands << "chown #{mount_uid}:#{mount_gid} #{guest_path}"
-            chown_commands << "chown #{mount_uid}:#{mount_gid_old} #{guest_path}"
-
-            exit_status = machine.communicate.sudo(chown_commands[0], error_check: false)
-            machine.communicate.sudo(chown_commands[1]) if exit_status != 0
+            chown_command = "chown #{mount_uid}:#{mount_gid} #{guest_path}"
+            machine.communicate.sudo(chown_command)
           end
-
-          # Emit an upstart event if we can
-          machine.communicate.sudo <<-EOH.gsub(/^ {12}/, "")
-            if command -v /sbin/init && /sbin/init --version | grep upstart; then
-              /sbin/initctl emit --no-wait vagrant-mounted MOUNTPOINT=#{guest_path}
-            fi
-          EOH
         end
 
         def self.unmount_virtualbox_shared_folder(machine, guestpath, options)
