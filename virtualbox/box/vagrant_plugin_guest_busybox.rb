7bdcb1dc4a13c902eb58b7da52f80b2aa707860d
A.I <ailis@paw.zone>
2017-01-26 08:21:27 +0900

    Fix #9
    based on https://github.com/mitchellh/vagrant/blob/master/plugins/guests/debian/cap/configure_networks.rb

--- virtualbox/box/vagrant_plugin_guest_busybox.rb
+++ virtualbox/box/vagrant_plugin_guest_busybox.rb
@@ -105,51 +105,66 @@ module VagrantPlugins
         include Vagrant::Util
 
         def self.configure_networks(machine, networks)
-          machine.communicate.tap do |comm|
-            # First, remove any previous network modifications
-            # from the interface file.
-            comm.sudo("sed -e '/^#VAGRANT-BEGIN/,$ d' /etc/network/interfaces > /tmp/vagrant-network-interfaces.pre")
-            comm.sudo("sed -ne '/^#VAGRANT-END/,$ p' /etc/network/interfaces | tail -n +2 > /tmp/vagrant-network-interfaces.post")
+          comm = machine.communicate
 
-            # Accumulate the configurations to add to the interfaces file as
-            # well as what interfaces we're actually configuring since we use that
-            # later.
-            interfaces = Set.new
-            entries = []
-            networks.each do |network|
-              interfaces.add(network[:interface])
-              network[:device] = "eth#{network[:interface]}"
-              entry = TemplateRenderer.render("guests/debian/network_#{network[:type]}",
-                                              options: network)
+          commands   = []
+          entries    = []
 
-              entries << entry
-            end
+          networks.each do |network|
+            network[:device] = "eth#{network[:interface]}"
 
-            # Perform the careful dance necessary to reconfigure
-            # the network interfaces
-            temp = Tempfile.new("vagrant")
-            temp.binmode
-            temp.write(entries.join("\n"))
-            temp.close
-
-            comm.upload(temp.path, "/tmp/vagrant-network-entry")
-
-            # Bring down all the interfaces we're reconfiguring. By bringing down
-            # each specifically, we avoid reconfiguring eth0 (the NAT interface) so
-            # SSH never dies.
-            interfaces.each do |interface|
-              comm.sudo("/sbin/ifdown eth#{interface} 2> /dev/null")
-              comm.sudo("/sbin/ip addr flush dev eth#{interface} 2> /dev/null")
-            end
-
-            comm.sudo('cat /tmp/vagrant-network-interfaces.pre /tmp/vagrant-network-entry /tmp/vagrant-network-interfaces.post > /etc/network/interfaces')
-            comm.sudo('rm -f /tmp/vagrant-network-interfaces.pre /tmp/vagrant-network-entry /tmp/vagrant-network-interfaces.post')
-
-            # Bring back up each network interface, reconfigured
-            interfaces.each do |interface|
-              comm.sudo("/sbin/ifup eth#{interface}")
-            end
+            entry = TemplateRenderer.render("guests/debian/network_#{network[:type]}",
+              options: network,
+            )
+            entries << entry
           end
+
+          Tempfile.open("vagrant-barge-configure-networks") do |f|
+            f.binmode
+            f.write(entries.join("\n"))
+            f.fsync
+            f.close
+            comm.upload(f.path, "/tmp/vagrant-network-entry")
+          end
+
+          networks.each do |network|
+            # Ubuntu 16.04+ returns an error when downing an interface that
+            # does not exist. The `|| true` preserves the behavior that older
+            # Ubuntu versions exhibit and Vagrant expects (GH-7155)
+            commands << "/sbin/ifdown '#{network[:device]}' || true"
+            commands << "/sbin/ip addr flush dev '#{network[:device]}'"
+          end
+
+          # Reconfigure /etc/network/interfaces.
+          commands << <<-EOH.gsub(/^ {12}/, "")
+            # Remove any previous network modifications from the interfaces file
+            sed -e '/^#VAGRANT-BEGIN/,$ d' /etc/network/interfaces \\
+              > /tmp/vagrant-network-interfaces.pre
+            sed -ne '/^#VAGRANT-END/,$ p' /etc/network/interfaces | \\
+              awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' | \\
+              sed -e '/^#VAGRANT-END/,$ d' | \\
+              awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' \\
+              > /tmp/vagrant-network-interfaces.post
+
+            cat \\
+              /tmp/vagrant-network-interfaces.pre \\
+              /tmp/vagrant-network-entry \\
+              /tmp/vagrant-network-interfaces.post \\
+              > /etc/network/interfaces
+
+            rm -f /tmp/vagrant-network-interfaces.pre
+            rm -f /tmp/vagrant-network-entry
+            rm -f /tmp/vagrant-network-interfaces.post
+          EOH
+
+          # Bring back up each network interface, reconfigured.
+          networks.each do |network|
+            commands << "/sbin/ifup '#{network[:device]}'"
+          end
+
+          # Run all the commands in one session to prevent partial configuration
+          # due to a severed network.
+          comm.sudo(commands.join("\n"))
         end
       end
     end
